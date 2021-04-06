59f9772f862dde3525508f4db5ecd7ecc2869db3
Drew <dreww@mortgage365.com>
2021-04-06 11:58:25 -0600

    186

--- ThreadingE/Program.cs
+++ ThreadingE/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ThreadingE
 {
@@ -7,39 +8,48 @@ namespace ThreadingE
     {
         static void Main(string[] args)
         {
-          /*  Console.WriteLine("Hello World! 1");
-            Thread.Sleep(1000);
-            Console.WriteLine("Hello World! 2");
-            Thread.Sleep(1000);
-            Console.WriteLine("Hello World! 3");
-            Thread.Sleep(1000);
-            Console.WriteLine("Hello World! 4"); */
+            /*  Console.WriteLine("Hello World! 1");
+              Thread.Sleep(1000);
+              Console.WriteLine("Hello World! 2");
+              Thread.Sleep(1000);
+              Console.WriteLine("Hello World! 3");
+              Thread.Sleep(1000);
+              Console.WriteLine("Hello World! 4"); */
 
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("thread 1");
-            }).Start();
-            
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("thread 2");
-            }).Start();
-            
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("thread 3");
-            }).Start();
-            
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("thread 4");
-            }).Start();
+            /* new Thread(() =>
+             {
+                 Thread.Sleep(1000);
+                 Console.WriteLine("thread 1");
+             }).Start();
 
-            Console.ReadLine();
+             new Thread(() =>
+             {
+                 Thread.Sleep(1000);
+                 Console.WriteLine("thread 2");
+             }).Start();
+
+             new Thread(() =>
+             {
+                 Thread.Sleep(1000);
+                 Console.WriteLine("thread 3");
+             }).Start();
+
+             new Thread(() =>
+             {
+                 Thread.Sleep(1000);
+                 Console.WriteLine("thread 4");
+             }).Start(); */
+
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            var thread = new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                taskCompletionSource.TrySetResult(true);
+            });
+            Console.WriteLine($"Thread number: {thread.ManagedThreadId}");
+            thread.Start();
+            var test = taskCompletionSource.Task.Result;
         }
     }
 }
