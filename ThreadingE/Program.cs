dfdf484a9acfdcb737ea3dfa1a2a9c20d0569609
Drew <dreww@mortgage365.com>
2021-04-06 11:52:39 -0600

    185

--- ThreadingE/Program.cs
+++ ThreadingE/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ThreadingE
 {
@@ -6,7 +7,39 @@ namespace ThreadingE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          /*  Console.WriteLine("Hello World! 1");
+            Thread.Sleep(1000);
+            Console.WriteLine("Hello World! 2");
+            Thread.Sleep(1000);
+            Console.WriteLine("Hello World! 3");
+            Thread.Sleep(1000);
+            Console.WriteLine("Hello World! 4"); */
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("thread 1");
+            }).Start();
+            
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("thread 2");
+            }).Start();
+            
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("thread 3");
+            }).Start();
+            
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("thread 4");
+            }).Start();
+
+            Console.ReadLine();
         }
     }
 }
